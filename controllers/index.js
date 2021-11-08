a8913d6bbcbd8473a545a9e0f1bacef12fb54085
mibrahim234 <mohamm1417@gmail.com>
2021-11-08 10:41:05 -0500

    file

--- controllers/index.js
+++ controllers/index.js
@@ -1,16 +1,10 @@
 // here we are collecting the packaged group of API endpoints and prefixing them with the path /api
 const router = require('express').Router();
-const apiRoutes = require('./api');
 
-router.use('/api', apiRoutes);
-
-router.use((req, res) => {
-  res.status(404).end();
-});
-
-// for the homeroute file 
+const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
 
 router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,27 +1,32 @@
-const express = require('express');
-const routes = require('./controllers/');
-// sequelize connection
-const sequelize = require('./config/connection');
 const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sequelize = require('./config/connection');
+
+const hbs = exphbs.create({});
+//require handlebars
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 // get files from public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// require handlebars
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-// turn on routes
-app.use(routes);
+const routes = require('./controllers/');
 
 // turn on connection to db and server
-// method to establish the connection to the database. The "sync" part means that this is Sequelize taking the models and connecting them to associated database tables. If it doesn't find a table, it'll create it for you!
+// method to establish the connection to the database. 
+//The "sync" part means that this is Sequelize taking the models and connecting them to associated database tables. 
+// If it doesn't find a table, it'll create it for you!
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
+
+
+
+
